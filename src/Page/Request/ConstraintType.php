3c438d975406f306bd000fd69b8db4d90877b625
Mario Schillermann <mario.schillermann@gmail.com>
2024-11-07 21:35:47 +0100

    extended constraint types

--- src/Page/Request/ConstraintType.php
+++ src/Page/Request/ConstraintType.php
@@ -5,6 +5,10 @@ namespace PhpPages\Page\Request;
 class ConstraintType implements ConstraintInterface
 {
     const TYPE_STRING = 'string';
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_FLOAT = 'float';
+    const TYPE_NUMERIC = 'numeric';
 
     private string $error;
     function __construct(private string $type)
@@ -14,8 +18,24 @@ class ConstraintType implements ConstraintInterface
 
     function check(string $propertyName, $propertyValue): void
     {
-        if ($this->type === self::TYPE_STRING) {
-            $this->error = is_string($propertyValue) ? '' : sprintf("Property '%s' is not a %s.", $propertyName, self::TYPE_STRING);
+        if (self::TYPE_STRING === $this->type) {
+            $this->error = is_string($propertyValue) ? '' : sprintf("Property '%s' is not a string.", $propertyName);
+            return;
+        }
+        if (self::TYPE_BOOLEAN === $this->type) {
+            $this->error = is_bool($propertyValue) ? '' : sprintf("Property '%s' is not a boolean.", $propertyName);
+            return;
+        }
+        if (self::TYPE_INTEGER === $this->type) {
+            $this->error = is_integer($propertyValue) ? '' : sprintf("Property '%s' is not an integer.", $propertyName);
+            return;
+        }
+        if (self::TYPE_FLOAT === $this->type) {
+            $this->error = is_float($propertyValue) ? '' : sprintf("Property '%s' is not a float.", $propertyName);
+            return;
+        }
+        if (self::TYPE_NUMERIC === $this->type) {
+            $this->error = is_numeric($propertyValue) ? '' : sprintf("Property '%s' is not a numeric.", $propertyName);
             return;
         }
         $this->error = sprintf("Property '%s' has an unknown type.", $propertyName);
--- tests/Page/Request/ConstraintTypeTest.php
+++ tests/Page/Request/ConstraintTypeTest.php
@@ -25,9 +25,141 @@ class ConstraintTypeTest extends TestCase
         $this->assertEquals("Property 'property' is not a string.", $constraintType->error());
     }
 
-    function testUnkownStringType(): void
+    function testBooleanType(): void
     {
-        $constraintType = new ConstraintType('unkown');
+        $constraintType = new ConstraintType(ConstraintType::TYPE_BOOLEAN);
+        $constraintType->check('property', true);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+    }
+
+    function testWrongBooleanType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_BOOLEAN);
+        $constraintType->check('property', 10);
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a boolean.", $constraintType->error());
+    }
+
+    function testIntegerType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_INTEGER);
+        $constraintType->check('property', 10);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+    }
+
+    function testWrongIntegerType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_INTEGER);
+        $constraintType->check('property', 'value');
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not an integer.", $constraintType->error());
+    }
+
+    function testFloatType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_FLOAT);
+        $constraintType->check('property', 5.1);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+    }
+
+    function testWrongFloatType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_FLOAT);
+        $constraintType->check('property', 10);
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a float.", $constraintType->error());
+    }
+
+    function testNumericType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', "42");
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 1337);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 0x539);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 0b10100111001);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 1337e0);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 9.1);
+
+        $this->assertFalse($constraintType->hasError());
+        $this->assertEmpty($constraintType->error());
+    }
+
+    function testWrongNumericType(): void
+    {
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', '0x539');
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', '0b10100111001');
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', 'not numeric');
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', []);
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', null);
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+
+        $constraintType = new ConstraintType(ConstraintType::TYPE_NUMERIC);
+        $constraintType->check('property', '');
+
+        $this->assertTrue($constraintType->hasError());
+        $this->assertEquals("Property 'property' is not a numeric.", $constraintType->error());
+    }
+
+    function testUnknownType(): void
+    {
+        $constraintType = new ConstraintType('unknown');
         $constraintType->check('property', 'value');
 
         $this->assertTrue($constraintType->hasError());
